5e92cd99759d662ef2069041c0af909da7783e6b
Emmanuel Adeniran <Adeniranbanjo07@gmail.com>
2026-05-10 08:39:18 +0100

    Second Commit

---
+++ src/FAQs/ReverseNumber.java
@@ -0,0 +1,10 @@
+package FAQs;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/FAQs/SwapNumbers.java
+++ src/FAQs/SwapNumbers.java
@@ -6,11 +6,26 @@ public class SwapNumbers {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter Number");
-		
+		System.out.println("Enter First Number: ");
 		int a = sc.nextInt();
-		System.out.println("The Number Entered is: "+ a);
-
+		System.out.println("Enter Second Number: ");
+		int b = sc.nextInt();
+		
+		// Approach 1 - Using a Third Number
+		
+		System.out.println("The Number before Swapping is: a="+a+" and b="+b);
+		int t =a;
+		a=b;
+		b=t;
+		System.out.println("The Number after Swapping is: a="+a+" and b="+b);
+		
+		// Approach 2 - using + and - Operator
+		System.out.println("The Number before Swapping is: a="+a+" and b="+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("The Number after Swapping is: a="+a+" and b="+b);
+				
 	}
 
 }
