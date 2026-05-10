84e31d80e1ef754374a4aa495dbd5f95593020bf
Emmanuel Adeniran <Adeniranbanjo07@gmail.com>
2026-05-10 08:29:44 +0100

    First Commit

---
+++ src/FAQs/SwapNumbers.java
@@ -0,0 +1,16 @@
+package FAQs;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Number");
+		
+		int a = sc.nextInt();
+		System.out.println("The Number Entered is: "+ a);
+
+	}
+
+}
